7142257117fc8fb6c30a3f37d65934e2a320759d
Ren Xiaojun <xjren@thoughtworks.com>
2011-12-11 21:54:41 +0800

    add scala vim plugin

--- peaches.rb
+++ peaches.rb
@@ -1,27 +1,37 @@
-def discard_one_and_take_20_percents(peaches, left_times)  
+
+
+def discard_one_and_take_20_percents(peaches, total_times, left_times)  
   if(left_times == 0)
     return peaches
   end
   
-  if(peaches % 5 == 1) 
-    return discard_one_and_take_20_percents((peaches - 1) / 5 * 4, left_times - 1)
+  #TODO extract method
+  if(peaches % total_times == 1) 
+    return discard_one_and_take_20_percents((peaches - 1) / total_times * (total_times - 1), left_times - 1)
   else
+    #TODO raise exception
     return -1
   end
 end
 
-left_times = 5
 left_peaches = 0
-
 not_found = true
+total_times = 7
 i = 0
 
 while(not_found)
-  left_peaches  = discard_one_and_take_20_percents(i ,left_times)
+  left_peaches  = discard_one_and_take_20_percents(i, total_times, left_times)
   if(left_peaches > 0)
     not_found = false
+
+    #TODO extract output method
     puts "sum: #{i}"
     puts "left: #{left_peaches}"
+    
   end
+  
+  #TODO increase by (total_times - 1)
   i = i + 1
 end
+
+#TODO add test case
